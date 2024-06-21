7ce8ad030fdd79b1d176a16165d88086487954eb
Wishwa-Subhasingha <60093220+Wishwa-code@users.noreply.github.com>
2024-06-21 09:58:34 +0530

    added new libraries

--- startserver.sh
+++ startserver.sh
@@ -1,6 +1,6 @@
 
 # Install required Python packages
-pip install flask flask_cors deep_translator transformers scikit-learn sentencepiece pdfplumber nltk datasets pdfminer.six spacy Pillow pymongo python-dotenv || exit
+pip install flask flask_cors deep_translator transformers scikit-learn sentencepiece pdfplumber nltk datasets pdfminer.six spacy Pillow pymongo python-dotenv botocore boto3 PyPDF2 flask_jwt_extended || exit
 python3 -m spacy download en_core_web_sm
 
 # Run the main Python script
